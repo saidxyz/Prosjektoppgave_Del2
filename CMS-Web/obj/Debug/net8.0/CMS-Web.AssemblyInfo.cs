696aa60ecde4fe1783c3d0a73fbba4a5c19172eb
Said Nasser <90267986+saidxyz@users.noreply.github.com>
2024-12-07 06:18:46 +0100

    update

--- CMS-Web/obj/Debug/net8.0/CMS-Web.AssemblyInfo.cs
+++ CMS-Web/obj/Debug/net8.0/CMS-Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS-Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50cfefd5e7411fa9e0665b89a477950e8bedad40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2457121f6c103061122c8674d32dfc8519c0f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS-Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS-Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
